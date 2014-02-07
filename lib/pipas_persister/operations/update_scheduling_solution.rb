5ed7505e3d07eabf83d8134b4051bd0b373d46db
Bernard Lambeau <blambeau@gmail.com>
2014-02-07 14:33:37 +0100

    Refactor UpdateSchedulingSolution, removed `fixed` and `last_xxx`.

--- lib/pipas_persister/operations/update_scheduling_solution.rb
+++ lib/pipas_persister/operations/update_scheduling_solution.rb
@@ -1,6 +1,6 @@
 module PipasPersister
   class Operation
-    class UpdateSchedulingOperation < Operation
+    class UpdateSchedulingSolution < Operation
 
       precondition "non obsolete problem" do |input|
         not relvar{
@@ -9,21 +9,35 @@ module PipasPersister
       end
 
       def execute(input)
+        # First set the appointment tuples
         relvar{
           base.appointments
         }.affect appointment_tuples(input)
+
+        # now touch the last_scheduled flag
+        relvar{
+          base.scheduling
+        }.update(last_scheduled: Time.now)
       end
 
     private
 
       def appointment_tuples(input)
+        old_fixed = get_old_fixed
         input.treatments
              .ungroup(:appointments)
              .allbut([:rdi])
              .extend(
-          appointment_id: ->(t){ PipasPersister::UUID_GENERATOR.generate })
+          appointment_id: ->(t){ PipasPersister::UUID_GENERATOR.generate },
+          fixed: ->(t){ old_fixed[[t.treatment_id, t.step_id]] || false })
       end
 
-    end # class AddTreatment
+      def get_old_fixed
+        query{
+          project(base.appointments, [:treatment_id, :step_id, :fixed])
+        }.to_hash([:treatment_id, :step_id] => :fixed)
+      end
+
+    end # class UpdateSchedulingSolution
   end # class Operation
 end # module PipasPersister
--- test/unit/operations/test_update_scheduling_solution.rb
+++ test/unit/operations/test_update_scheduling_solution.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 module PipasPersister
   class Operation
-    describe UpdateSchedulingOperation do
+    describe UpdateSchedulingSolution do
 
       before do
         @connection = PipasPersister::ALF_DATABASE.connection
@@ -14,7 +14,7 @@ module PipasPersister
       end
 
       subject{
-        UpdateSchedulingOperation.new(@connection).call(input)
+        UpdateSchedulingSolution.new(@connection).call(input)
       }
 
       context 'when the problem key is obsolete' do
@@ -30,7 +30,29 @@ module PipasPersister
       end
 
       context 'when the problem key is not obsolete' do
-        let(:appointments){
+        let(:input){
+          Tuple(
+            problem_key: "update-scheduling-solution",
+            rdi: 0.95,
+            treatments: Relation([
+              {
+                treatment_id: "d9027510-66ff-0131-38cb-3c07545ed162",
+                appointments: Relation([
+                  {
+                    step_id: "74685ae0-66fd-0131-38cb-3c07545ed162",
+                    scheduled_at: Time.parse("2014-01-25 09:00:00")
+                  },
+                  {
+                    step_id: "74685c90-66fd-0131-38cb-3c07545ed162",
+                    scheduled_at: Time.parse("2014-02-25 15:00:00")
+                  }
+                ])
+              }
+            ])
+          )
+        }
+
+        let(:expected_after){
           Relation([
             {
               step_id: "74685ae0-66fd-0131-38cb-3c07545ed162",
@@ -44,18 +66,6 @@ module PipasPersister
             }
           ])
         }
-        let(:input){
-          Tuple(
-            problem_key: "update-scheduling-solution",
-            rdi: 0.95,
-            treatments: Relation([
-              {
-                treatment_id: "d9027510-66ff-0131-38cb-3c07545ed162",
-                appointments: appointments
-              }
-            ])
-          )
-        }
 
         it 'should set the new appointments' do
           subject
@@ -64,7 +74,7 @@ module PipasPersister
               restrict(base.appointments,
                 treatment_id: "d9027510-66ff-0131-38cb-3c07545ed162"),
               [:step_id, :fixed, :scheduled_at])
-          }.should eq(appointments)
+          }.should eq(expected_after)
         end
       end
 
